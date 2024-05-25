b51cb4d9cd32b8d8c8f246ce52cb651d5bf21f6c
Miguel <rosadomiguel2305@gmail.com>
2024-05-24 18:13:21 -0600

    repository2

---
+++ map/script.js
@@ -0,0 +1,25 @@
+let map;
+let geocoder;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+    geocoder = new google.maps.Geocoder();
+}
+
+function geocodeAddress() {
+    const address = document.getElementById('direccion').value;
+    geocoder.geocode({ 'address': address }, function(results, status) {
+        if (status === 'OK') {
+            map.setCenter(results[0].geometry.location);
+            new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
